e698b8bc5d08e695888981962ae68078c1d36c94
Brahim Bahaida <bbahieda@gmail.com>
2018-03-03 20:52:07 +0100

    Added project skeleton

---
+++ src/main/java/com/brahim/springfull/SpringFullApplication.java
@@ -0,0 +1,12 @@
+package com.brahim.springfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFullApplication.class, args);
+	}
+}
---
+++ src/test/java/com/brahim/springfull/SpringFullApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brahim.springfull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringFullApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
